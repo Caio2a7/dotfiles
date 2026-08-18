0625ebf368775a6fd368abeceb253ce7804a6e9d
Caio2a7 <caiodanielfonseca@gmail.com>
2026-08-18 00:00:21 -0300

    feat(dap): add nvim-dap-virtual-text and nvim-dap-ui configuration

--- nvim/.config/nvim/lua/plugins/languages/dap.lua
+++ nvim/.config/nvim/lua/plugins/languages/dap.lua
@@ -1,24 +1,64 @@
 return {
-  "mfussenegger/nvim-dap",
-  config = function()
-    local dap = require("dap")
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
 
-    dap.adapters.python = {
-      type = "executable",
-      command = "python",
-      args = { "-m", "debugpy.adapter" },
-    }
+      dap.adapters.python = {
+        type = "executable",
+        command = "python",
+        args = { "-m", "debugpy.adapter" },
+      }
 
-    dap.configurations.python = {
-      {
-        type = "python",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}",
-        pythonPath = function()
-          return "/usr/bin/python"
-        end,
-      },
-    }
-  end,
+      dap.configurations.python = {
+        {
+          type = "python",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          pythonPath = function()
+            return "/usr/bin/python"
+          end,
+        },
+      }
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap" },
+    opts = {
+      enabled = true,
+      enable_commands = true,
+      all_frames = false,
+      virt_text_pos = "eol",
+      commented = true,
+      highlight_changed_variables = true,
+      highlight_new_as_changed = true,
+      show_stop_reason = true,
+      display_callback = function(variable)
+        return "  " .. variable.name .. " = " .. variable.value:gsub("%s+", " ")
+      end,
+    },
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup(opts)
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        pcall(dapui.open)
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        pcall(dapui.close)
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        pcall(dapui.close)
+      end
+    end,
+  },
 }
