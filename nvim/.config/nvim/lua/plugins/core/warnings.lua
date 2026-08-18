4ea15a60c1d2fb7c5952b2a5c14a7bb583b33a4a
Caio2a7 <caiodanielfonseca@gmail.com>
2026-08-18 00:00:22 -0300

    feat(core): enable diagnostics by default and sync inlay hints and DAP virtual text on Ctrl+d

--- nvim/.config/nvim/lua/plugins/core/warnings.lua
+++ nvim/.config/nvim/lua/plugins/core/warnings.lua
@@ -1,9 +1,12 @@
-local diagnostics_visible = false
+local diagnostics_visible = true
 
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = false,
-  underline = false,
+  virtual_text = {
+    prefix = "●",
+    spacing = 4,
+  },
+  signs = true,
+  underline = true,
   update_in_insert = false,
   severity_sort = true,
 })
@@ -21,7 +24,13 @@ local function toggle_diagnostics()
       update_in_insert = false,
       severity_sort = true,
     })
-    vim.notify("Diagnósticos ativados", vim.log.levels.INFO)
+    if vim.lsp.inlay_hint then
+      pcall(vim.lsp.inlay_hint.enable, true)
+    end
+    pcall(function()
+      require("nvim-dap-virtual-text").enable()
+    end)
+    vim.notify("Modo Debug / Diagnósticos ATIVADOS", vim.log.levels.INFO)
   else
     vim.diagnostic.config({
       virtual_text = false,
@@ -29,7 +38,13 @@ local function toggle_diagnostics()
       underline = false,
       update_in_insert = false,
     })
-    vim.notify("Diagnósticos desativados", vim.log.levels.INFO)
+    if vim.lsp.inlay_hint then
+      pcall(vim.lsp.inlay_hint.enable, false)
+    end
+    pcall(function()
+      require("nvim-dap-virtual-text").disable()
+    end)
+    vim.notify("Modo Debug / Diagnósticos DESATIVADOS", vim.log.levels.INFO)
   end
 end
 
@@ -39,6 +54,6 @@ vim.keymap.set({ "n", "i", "v" }, "<C-d>", function()
   if is_insert then
     pcall(vim.cmd, "startinsert")
   end
-end, { desc = "Toggle diagnósticos (Ctrl+d)", noremap = true, silent = true, nowait = true })
+end, { desc = "Toggle modo debug/diagnósticos (Ctrl+d)", noremap = true, silent = true, nowait = true })
 
 return {}
