6d3084b89cff4e6da54c1ce172285ea332f2bc1d
Caio2a7 <caiodanielfonseca@gmail.com>
2026-09-01 07:50:27 -0300

    fix(java): restore lazyvim java extra

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.java" },
 
     -- 🎯 INJEÇÃO DO PLUGIN OIL.NVIM (GARANTIDA DE FUNCIONAMENTO)
     {
