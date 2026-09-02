3507ba170620d734fa362e135730057de54d9905
Caio2a7 <caiodanielfonseca@gmail.com>
2026-09-02 18:05:00 -0300

    chore(nvim): add mermaid parser to treesitter config

--- nvim/.config/nvim/lua/plugins/languages/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/languages/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "markdown", "markdown_inline", "lua", "vim", "vimdoc" },
+    ensure_installed = { "markdown", "markdown_inline", "lua", "vim", "vimdoc", "mermaid" },
     auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
